6c760fff1e7172788cd63f64ab787ef3c852fbdf
icicche <ivo.cicchese@iiscurielevi.it>
2025-10-06 16:17:03 +0200

    Create 06-even.py

---
+++ 001/06-even.py
@@ -0,0 +1,10 @@
+# 001/06
+# Questo script controlla se un numero inserito
+# da tastiera è pari o dispari
+
+n = int(input("Inserisci un numero intero: "))
+
+if n % 2 == 0:
+  print("Il numero inserito è pari")
+else:
+  print("Il numero inserito è dispari")
