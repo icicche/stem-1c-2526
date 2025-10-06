e8d7946ee55256c770da7c7d25f7933b9e1ec415
icicche <ivo.cicchese@iiscurielevi.it>
2025-10-06 16:14:09 +0200

    Create 05-esercizio-2.py

---
+++ 001/05-esercizio-2.py
@@ -0,0 +1,8 @@
+# 001/05
+# Esercizio
+#
+# Crea uno script python che
+# 1. Chiede all'utente di inserire una stringa
+# 2. Se la lunghezza è > 10 stampa "La stringa è lunga"
+#    Se la lunghezza è < 5  stampa "La stringa è corta"
+#    Altrimenti stampa "La stringa non è lunga né corta"
