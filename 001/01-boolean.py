2ba888450802d7ca579bc7588d9ec193f8b0b303
icicche <236392917+icicche@users.noreply.github.com>
2025-10-06 18:00:11 +0200

    Better highlight

--- 001/01-boolean.py
+++ 001/01-boolean.py
@@ -10,10 +10,10 @@ b = a > 3
 # b = False se a NON è maggiore di 3
 #
 # Altri possibili valori di b sono
-# b = a == 5 (significa a uguale a 5)
+# b = a == 5 (significa "a uguale a 5")
 # b = a <= 4
 # b = a >= 2
-# b = a != 1 (significa a diverso da 1)
+# b = a != 1 (significa a "diverso da 1")
 
 print("Valore della variabile b:")
 print(b) # False
