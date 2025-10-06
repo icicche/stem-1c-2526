841ce4ad164824b7a9e614c3a2d37ab280dde0ef
icicche <ivo.cicchese@iiscurielevi.it>
2025-10-06 16:23:30 +0200

    Create 07-esercizio-3.py

---
+++ 001/07-esercizio-3.py
@@ -0,0 +1,17 @@
+# 001/07
+# Esercizio
+#
+# Scrivere uno script che
+# 1. Legge un numero intero da tastiera
+# 2. Controlla se è un multiplo di 8 E/O è un multiplo di 13
+#
+# Esempi di funzionamento:
+#
+# Se inseriamo il numero 416, lo script deve stampare:
+# "Il numero 416 è un multiplo di 8 e di 13"
+#
+# Se inseriamo il numero 16, lo script deve stampare:
+# "Il numero 16 è un multiplo di 8 ma non di 13"
+#
+# Se inseriamo il numero 92, lo script deve stampare:
+# "Il numero 92 non è un multiplo di 8 né di 13"
