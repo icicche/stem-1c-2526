756b58e9048836495750e6b406fa75e40ec4bce5
icicche <ivo.cicchese@iiscurielevi.it>
2025-10-06 16:11:01 +0200

    Create 04-esercizio-1.py

---
+++ 001/04-esercizio-1.py
@@ -0,0 +1,8 @@
+# 001/04
+# Esercizio
+#
+# Crea uno script python che
+# 1. Chiede all'utente di inserire una stringa
+# 2. Stampa a schermo il numero di caratteri (la lunghezza) della stringa inserita
+#
+# N.B. Ricorda che se s è una stringa la sua lunghezza è len(s)
