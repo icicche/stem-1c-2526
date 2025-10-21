80b294b9537d85106805465dd3297f298e1df906
icicche <236392917+icicche@users.noreply.github.com>
2025-10-21 16:08:03 +0200

    Add solutions to 002

--- 002/04-esercizio-1.py
+++ 002/04-esercizio-1.py
@@ -3,3 +3,23 @@
 #
 # Creare uno script che stampa i DIVISORI di un numero intero inserito
 # da tastiera e riporta il numero totale di divisori
+
+print("Questo script calcola i divisori di un numero.")
+
+n = int(input("Inserisci un intero positivo: "))
+
+d = 1
+totale = 0
+
+print("\nDivisori di " + str(n) + "\n---")
+
+while d <= n:
+
+    if n % d == 0:
+    # se d è un divisore di n:
+        print(d)
+        totale = totale + 1
+
+    d = d + 1
+
+print("---\nTotale: " + str(totale))
--- 002/08-esercizio-2.py
+++ 002/08-esercizio-2.py
@@ -12,3 +12,31 @@
 # Scrivere una variazione del gioco in cui a ogni tentativo
 # inserito lo script comunica se il numero da indovinare è
 # maggiore o minore del numero inserito
+
+import random
+
+num_segreto = random.randint(0,20)
+max_tentativi = 5
+
+print("Ho scelto casualmente un numero intero compreso tra 0 e 20.")
+print("Hai " + str(max_tentativi) + " tentativi per indovinarlo...")
+
+partita_vinta = False
+tentativo = 1
+
+while not partita_vinta and tentativo <= max_tentativi:
+    n = int(input("\nTentativo " + str(tentativo) + ": "))
+
+    if n < num_segreto:
+        print("Il numero da indovinare è più grande.")
+    elif n > num_segreto:
+        print("Il numero da indovinare è più piccolo.")
+    else:
+        partita_vinta = True
+    
+    tentativo = tentativo + 1
+
+if partita_vinta:
+    print("\nCOMPLIMENTI, hai indovinato!")
+else:
+    print("\nGAME OVER, il numero da indovinare era " + str(num_segreto) + ".")
